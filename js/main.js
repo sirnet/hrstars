d65d1906c656bdf342b014d2db23886549ded3e5
sirnet <artem@home-pc>
2020-08-01 20:57:04 +0300

    main section partners

--- js/main.js
+++ js/main.js
@@ -7,3 +7,20 @@ $(".mob-menu__list").click(function(){
     $(".header__contacts").addClass("header__contacts--active");
     $(".header-shadow").addClass("header-shadow--active");
 });
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
